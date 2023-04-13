374ac8e23da94092469da9275e3ff39e61fd7890
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-04-13 08:38:05 +0500

    :sparkles: added product categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { CategoriesSectionComponent } from './components/categories-section/categories-section.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    CategoriesSectionComponent
   ],
   imports: [
     BrowserModule,
